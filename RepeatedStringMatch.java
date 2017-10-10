8b8c5527ca347799876f791c1c85674eabf9851d
DouglasChan <douglas.chan.utoronto@outlook.com>
2017-10-09 23:51:08 -0400

    Create RepeatedStringMatch.java

---
+++ RepeatedStringMatch.java
@@ -0,0 +1,35 @@
+/*
+Given two strings A and B, find the minimum number of times A has to be repeated such that B is a substring of it. If no such solution, return -1.
+
+For example, with A = "abcd" and B = "cdabcdab".
+
+Return 3, because by repeating A three times (“abcdabcdabcd”), B is a substring of it; and B is not a substring of A repeated two times ("abcdabcd").
+
+Note:
+The length of A and B will be between 1 and 10000.
+
+*/
+
+
+class RepeatedStringMatch {
+    public int repeatedStringMatch(String A, String B) {
+        if (A.isEmpty())
+            return -1;
+        if (B.isEmpty())
+            return 0;
+        int repeatTimes =0;
+        //int MaxRepeatTimes = Math.floor();
+        while (repeatTimes  <= 10000 / A.length()) {
+            if (A.contains(B))
+                return repeatTimes;
+            else {
+                A = A + A;
+                repeatTimes += 1;   
+            }
+            
+        }
+        return -1;
+              
+        
+    }
+}
