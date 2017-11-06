74590ccbadb16c1f5795cd096436112bcbaf7c2a
DouglasChen <douglas.chenh@outlook.com>
2017-11-05 19:11:05 -0500

    Create PowerOfThree.java

---
+++ PowerOfThree.java
@@ -0,0 +1,10 @@
+//Given an integer, write a function to determine if it is a power of three.
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n>1)
+            while(n%3==0) 
+                n /= 3;
+        return n==1;
+    }
+}
